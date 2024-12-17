7f1ef41fb2e5b00e421d70361994444c094cafc2
andyftj <68372842+andyftj@users.noreply.github.com>
2024-12-17 15:19:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
-// Create the map
-    var map = L.map('map').setView([51.505, -0.09], 13); 
+var map = L.map('map').setView([ 43.6, 73.4, 13 );
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-    // Add a basemap tile layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+// Create layer objects (importantly, don't add them to the map yet)
+let roadsLayer = null
+fetch('map.geojson')
+    .then(response => response.json())
+    .then(data => {
+        roadsLayer = L.geoJSON(data)
+    })
 
-// Assuming your GeoJSON file is named 'my_data.geojson'
-    fetch('my_data1.json')
+let buildingsLayer = null
+fetch('map (1).geojson')
+    .then(response => response.json())
+    .then(data => {
+        buildingsLayer = L.geoJSON(data)
+    });
+let parksLayer = null
+
+fetch('map (2).geojson')
         .then(response => response.json())
-        .then(data => {
-            L.geoJSON(data).addTo(map); 
-        });
+    .then(data => {
+        parksLayer = L.geoJSON(data)
+    });
+
+// Add a simple toggle to the index.html to demonstrate
+    const container = document.getElementById('map').insertAdjacentHTML('afterend','<input id="toggleRoads" type="checkbox" name="roads"> <label for ="toggleRoads">Show Roads</label> <br ><input id="toggleBuildings" type="checkbox" name="buildings"> <label for ="toggleBuildings">Show Buildings</label> <br ><input id="toggleParks" type="checkbox" name="parks"> <label for ="toggleParks">Show Parks</label>')
+    
+    const toggleRoads = document.getElementById('toggleRoads')
+    const toggleBuildings = document.getElementById('toggleBuildings')
+    const toggleParks = document.getElementById('toggleParks')
+
+    toggleRoads.addEventListener('click', () => {
+      if(map.hasLayer(roadsLayer)){
+         map.removeLayer(roadsLayer)
+       }
+     else {
+          roadsLayer.addTo(map)
+        }
+      })
+    
+        toggleBuildings.addEventListener('click', () => {
+      if(map.hasLayer(buildingsLayer)){
+         map.removeLayer(buildingsLayer)
+       }
+     else {
+          buildingsLayer.addTo(map)
+        }
+      })
+
+        toggleParks.addEventListener('click', () => {
+      if(map.hasLayer(parksLayer)){
+         map.removeLayer(parksLayer)
+       }
+     else {
+          parksLayer.addTo(map)
+        }
+      })
