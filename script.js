43ec6ea93dd770cabad370250a3f66542b882286
andyftj <68372842+andyftj@users.noreply.github.com>
2024-12-17 14:46:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 // Create the map
-   var map = L.map('map').setView([51.505, -0.09], 13); 
+    var map = L.map('map').setView([ /* your latitude */, /* your longitude */ ], /* your zoom level */ ); 
 
-   // Add a basemap tile layer
-   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-   }).addTo(map);
-
-   // Load your GeoJSON data (replace 'custom.geo (1).json' with the actual filename)
-   fetch('custom.geo (1).json')
-       .then(response => response.json())
-       .then(data => {
-           // Add the GeoJSON layer to the map
-           L.geoJSON(data).addTo(map);
-       });
+    // Add a basemap tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
