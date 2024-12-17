a5d7e54fd0e5bb728e60e8a8a457ff8e745082ac
andyftj <68372842+andyftj@users.noreply.github.com>
2024-12-17 14:48:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+// Assuming your GeoJSON file is named 'my_data.geojson'
+    fetch('my_data.geojson')
+        .then(response => response.json())
+        .then(data => {
+            L.geoJSON(data).addTo(map); 
+        });
